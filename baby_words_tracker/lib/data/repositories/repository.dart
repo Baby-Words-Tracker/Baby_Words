15793be00f573dad15f0b31a6a2e02c9a5751ebd
cddale <cddale@crimson.ua.edu>
2025-01-30 20:04:41 -0600

    CRUD for repository

---
+++ baby_words_tracker/lib/data/repositories/repository.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+class repository {
+  final database = FirebaseFirestore.instance;
+
+  repository();
+
+  Future<String> create(String collectionName, Map<String, dynamic> data) async {
+    final collection = database.collection(collectionName);
+    final docRef = await collection.add(data);
+    return docRef.id;
+  }
+
+  Future<Map<String, dynamic>> read(String collectionName, String docId) async {
+    final docRef = database.collection(collectionName).doc(docId);
+    final doc = await docRef.get();
+    if (!doc.exists) {
+      throw Exception('Document not found');
+    }
+    return doc.data() as Map<String, dynamic>;
+  }
+
+  Future<void> update(String collectionName, String docId, Map<String, dynamic> data) async {
+    final docRef = database.collection(collectionName).doc(docId);
+    await docRef.update(data);
+  }
+
+  Future<void> delete(String collectionName, String docId) async {
+    final docRef = database.collection(collectionName).doc(docId);
+    await docRef.delete();
+  }
+
+  //isEqualTo
+  Future<List<Map<String, dynamic>>> query(String collectionName, String field, dynamic value) async {
+    final collection = database.collection(collectionName);
+    final snapshot = await collection.where(field, isEqualTo: value).get();
+    if (snapshot.docs.isEmpty) {
+      throw Exception('No matching documents.');
+    }
+    return snapshot.docs.map((doc) => {'id': doc.id, ...doc.data()}).toList();
+  }
+  
+}
