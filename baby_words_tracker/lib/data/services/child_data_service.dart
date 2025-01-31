31062df9a415e41e7b385d3c940fc87b82961c03
Marcelo Torres <mtorres3@crimson.ua.edu>
2025-01-31 16:05:25 -0600

    added child service with getChild call

---
+++ baby_words_tracker/lib/data/services/child_data_service.dart
@@ -0,0 +1,27 @@
+import 'package:baby_words_tracker/data/models/child.dart';
+import 'package:baby_words_tracker/data/repositories/FirestoreRepository.dart';
+
+class ChildDataService {
+
+  static final firebaseRepo = FirestoreRepository();
+
+  //child services
+  Future<String> createChild(DateTime cBirthDay, String cName, int cWordCount, List<String> cParentIDs) async {
+    final object = Child(birthday: cBirthDay, name: cName, wordCount: cWordCount, parentIDs: cParentIDs);
+    late String returnId;
+    await firebaseRepo.create("Child", object.toMap()).then(
+      (id) {
+        returnId = id;
+      }
+    );
+    return returnId; 
+  }
+
+  Future<Child> getChild(String id) async {
+    return Child.fromMap(await firebaseRepo.read("Child", id));
+  }
+
+
+
+  
+}
