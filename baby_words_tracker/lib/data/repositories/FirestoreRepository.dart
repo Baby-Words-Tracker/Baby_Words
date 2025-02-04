5d813c13cacd5b705b732fec8475d05e7d651eaa
cddale <cddale@crimson.ua.edu>
2025-02-04 14:31:35 -0600

    adding service to get all words from a given day

--- baby_words_tracker/lib/data/repositories/FirestoreRepository.dart
+++ baby_words_tracker/lib/data/repositories/FirestoreRepository.dart
@@ -107,6 +107,17 @@ class FirestoreRepository {
     return data;
   }
 
+  Future<List<DataWithId>> subQueryByField(String collectionName, String docId, String subcollection, String field, dynamic value) async {
+    final collection = database.collection(collectionName).doc(docId).collection(subcollection);
+    final snapshot = await collection.where(field, isEqualTo: value).get();
+    List<DataWithId> data = List.empty(growable: true);
+
+    for(DocumentSnapshot doc in snapshot.docs) {
+      data.add(DataWithId.fromFirestore(doc));
+    }
+    return data;
+  }
+
   Future<List<DataWithId>> fieldGreaterThan(String collectionName, String field, dynamic value) async {
     final collection = database.collection(collectionName);
     final querySnapshot = await collection.where(field, isGreaterThan: value).get();
