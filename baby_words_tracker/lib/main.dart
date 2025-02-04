096abcffeb2ef365862f265053f6f927e5c3603f
Gwynevere Deterding <gadeterding@crimson.ua.edu>
2025-02-04 22:30:31 +0000

    changed name of add_text for conventions

--- baby_words_tracker/lib/main.dart
+++ baby_words_tracker/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'pages/addtext.dart';
+import 'pages/add_text.dart';
 import 'pages/home_page.dart';
 import 'pages/stats.dart';
 
