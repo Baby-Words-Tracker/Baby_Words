12d3c966a2a58fc9de9818ca70b12c40035d8de7
Robert Powell <repowell1@crimson.ua.edu>
2025-02-06 13:58:58 -0600

    added scaffold for multiple language checking in spellcheck

--- baby_words_tracker/lib/util/spellcheck.dart
+++ baby_words_tracker/lib/util/spellcheck.dart
@@ -1,10 +1,11 @@
+import 'package:baby_words_tracker/util/language_code.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:http/http.dart' as http;
 import 'package:baby_words_tracker/data/services/word_data_service.dart';
 import 'package:baby_words_tracker/data/models/word.dart';
 
 //basic spellcheck that checks if a word exists in our word bank or in the englsh dictionary
-bool spellcheck(String word) {
+bool spellcheck(String word, {LanguageCode language = LanguageCode.en}) {
   final word_service = WordDataService();
   late Word? wordTest;
 
@@ -16,8 +17,15 @@ bool spellcheck(String word) {
   );
   if(wordTest != null) return true;
 
-  //check dictionary for word
-  final url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
+  String url = "";
+  //check dictionary for word, choose dictionary with switch
+  switch (language) {
+    case LanguageCode.en:
+      url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
+      break;
+    default:
+    return true; //no dictionary for this language, just accept it LMAO
+  }
   late http.Response response;
   http.get(Uri.parse(url)).then(
       (getResponse) {
