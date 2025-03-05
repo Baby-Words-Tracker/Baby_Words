040d949b224a062d295b75e31c4a89424320c362
Marcelo Torres <mtorres3@crimson.ua.edu>
2025-03-04 20:23:45 -0600

    updates

--- firebase-project/functions/index.js
+++ firebase-project/functions/index.js
@@ -1,19 +1,25 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
+const {logger} = require("firebase-functions");
+// const {onRequest} = require("firebase-functions/v2/https");
+// const {onDocumentCreated} = require("firebase-functions/v2/firestore");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// The Firebase Admin SDK to access Firestore.
+const {initializeApp} = require("firebase-admin/app");
+// const {getFirestore} = require("firebase-admin/firestore");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const {getAuth} = require("firebase-admin/auth");
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const {functions} = require("firebase-functions/v1/");
+
+initializeApp();
+
+exports.addDefaultClaim = functions.auth.user().onCreate(async (user) => {
+  try {
+    // Set the custom claim 'parent' to true
+    await getAuth().setCustomUserClaims(user.uid, {parent: true});
+
+    logger.log(`Custom claim set for user ${user.uid}`);
+  } catch (error) {
+    logger.error(`Error setting custom claim: ${error}`);
+  }
+});
