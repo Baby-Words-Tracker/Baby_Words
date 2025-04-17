21a49c71e40ea6e0fa7588bc32f58416570aad62
Marcelo Torres <mtorres3@crimson.ua.edu>
2025-04-17 17:17:20 -0500

    working role_testing page

--- baby_words_tracker/lib/util/download_as_csv.dart
+++ baby_words_tracker/lib/util/download_as_csv.dart
@@ -1,5 +1,7 @@
-import 'package:to_csv/to_csv.dart' as exportCSV;
+import 'package:to_csv/to_csv.dart' as export_csv;
 
-void downloadAsCSV(List<String> header, List<List<String>> dataList){
-   exportCSV.myCSV(header, dataList, setHeadersInFirstRow: true, fileName: "wordUtteranceData-");
+void downloadAsCSV(List<String> header, List<List<String>> dataList,
+    [String fileName = "wordUtteranceData-"]) {
+  export_csv.myCSV(header, dataList,
+      setHeadersInFirstRow: true, fileName: fileName);
 }
