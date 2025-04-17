54745337451e5890f326df08248415bc8a5bb95e
Marcelo Torres <mtorres3@crimson.ua.edu>
2025-04-16 21:39:22 -0500

    role_testing page works completely now (I still need to add the ability to download the uid-email table though)

--- baby_words_tracker/lib/pages/testing/role_testing.dart
+++ baby_words_tracker/lib/pages/testing/role_testing.dart
@@ -18,32 +18,13 @@ class _RoleTestingState extends State<RoleTesting> {
   Map<String, dynamic> _userRoles = {};
   dynamic _userData = [];
 
-  // Future<List<Map<String, String>>> _searchUsers(String query) async {
-  //   if (query.isEmpty) return [];
-
-  //   String collection = _selectedUserType == UserType.parent
-  //       ? Parent.collectionName
-  //       : Researcher.collectionName;
-  //   var snapshot = await FirebaseFirestore.instance
-  //       .collection(collection)
-  //       .where('email', isGreaterThanOrEqualTo: query)
-  //       .where('email', isLessThan: '${query}z')
-  //       .limit(5)
-  //       .get();
-
-  //   return snapshot.docs
-  //       .map((doc) => {'email': doc['email'] as String, 'uid': doc.id})
-  //       .toList();
-  // }
-
   Future<void> _callRoleFunction(String functionName) async {
     if (_selectedUserEmail == null) return;
-    debugPrint(
-        'Getting callable for function $functionName with uid $_selectedUserEmail');
+    // debugPrint('Getting callable for function $functionName with uid $_selectedUserEmail');
     HttpsCallable function =
         FirebaseFunctions.instance.httpsCallable(functionName);
     try {
-      debugPrint('Calling function $functionName with uid $_selectedUserEmail');
+      // debugPrint('Calling function $functionName with uid $_selectedUserEmail');
       final response = await function.call({'targetEmail': _selectedUserEmail});
       ScaffoldMessenger.of(context)
           .showSnackBar(SnackBar(content: Text(response.data['message'])));
@@ -68,90 +49,88 @@ class _RoleTestingState extends State<RoleTesting> {
                 labelText: 'Search User by Email',
               ),
               onChanged: (String value) async {
-                debugPrint("Setting value for $value");
+                // debugPrint("Setting value for $value");
                 setState(() {
                   _selectedUserEmail = value;
                   _userRoles = {};
-                  debugPrint("_selectedUserEmail set to $value");
+                  // debugPrint("_selectedUserEmail set to $value");
                 });
               },
             ),
-            if (_selectedUserEmail != null) ...[
-              Text('Selected User ID: $_selectedUserEmail'),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Text(" User Roles: ["),
-                  ..._userRoles.entries.map(
-                    (entry) {
-                      return Text("(${entry.key}: ${entry.value}), ");
-                    },
-                  ),
-                  const Text("]"),
-                ],
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  var customClaims = await callFunction(
-                    context,
-                    'getUserCustomClaims',
-                    {'targetEmail': _selectedUserEmail},
-                  );
-                  if (customClaims == null) {
-                    debugPrint("No Custom Claims");
-                    return;
-                  } else {
-                    setState(() {
-                      _userRoles = customClaims;
-                    });
-                  }
-                },
-                child: const Text("Get Custom Claims"),
-              ),
-              ElevatedButton(
-                  onPressed: () async {
-                    await _callRoleFunction('giveParentClaim');
+            Text('Selected User ID: $_selectedUserEmail'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(" User Roles: ["),
+                ..._userRoles.entries.map(
+                  (entry) {
+                    return Text("(${entry.key}: ${entry.value}), ");
                   },
-                  child: const Text("Assign Parent Role")),
-              ElevatedButton(
-                  onPressed: () => _callRoleFunction('removeParentClaim'),
-                  child: const Text("Remove Parent Role")),
-              ElevatedButton(
-                onPressed: () => _callRoleFunction('giveResearcherClaim'),
-                child: const Text('Assign Researcher Role'),
-              ),
-              ElevatedButton(
-                onPressed: () => _callRoleFunction('removeResearcherClaim'),
-                child: const Text('Remove Researcher Role'),
-              ),
-              ElevatedButton(
-                onPressed: () => _callRoleFunction('giveAdminClaim'),
-                child: const Text('Assign Admin Role'),
-              ),
-              ElevatedButton(
-                onPressed: () => _callRoleFunction('removeAdminClaim'),
-                child: const Text('Remove Admin Role'),
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  var userData =
-                      await callFunction(context, 'getEmailUIDTable', {});
-                  if (userData != null) {
-                    debugPrint("userData received: ${userData['users']}");
-                    setState(() {
-                      _userData = userData['users'];
-                    });
-                  } else {
-                    debugPrint("No user data received");
-                  }
-                },
-                child: const Text('Get Email-UID Data'),
-              ),
-              if (_userData.isNotEmpty) ...[
-                const Text('User Data:'),
-                ..._userData.map((data) => Text(data.toString())),
+                ),
+                const Text("]"),
               ],
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                var customClaims = await callFunction(
+                  context,
+                  'getUserCustomClaims',
+                  {'targetEmail': _selectedUserEmail},
+                );
+                if (customClaims == null) {
+                  // debugPrint("No Custom Claims");
+                  return;
+                } else {
+                  setState(() {
+                    _userRoles = customClaims;
+                  });
+                }
+              },
+              child: const Text("Get Custom Claims"),
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  await _callRoleFunction('giveParentClaim');
+                },
+                child: const Text("Assign Parent Role")),
+            ElevatedButton(
+                onPressed: () => _callRoleFunction('removeParentClaim'),
+                child: const Text("Remove Parent Role")),
+            ElevatedButton(
+              onPressed: () => _callRoleFunction('giveResearcherClaim'),
+              child: const Text('Assign Researcher Role'),
+            ),
+            ElevatedButton(
+              onPressed: () => _callRoleFunction('removeResearcherClaim'),
+              child: const Text('Remove Researcher Role'),
+            ),
+            ElevatedButton(
+              onPressed: () => _callRoleFunction('giveAdminClaim'),
+              child: const Text('Assign Admin Role'),
+            ),
+            ElevatedButton(
+              onPressed: () => _callRoleFunction('removeAdminClaim'),
+              child: const Text('Remove Admin Role'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                var userData =
+                    await callFunction(context, 'getEmailUIDTable', {});
+                if (userData != null) {
+                  // debugPrint("userData received: ${userData['users']}");
+                  setState(() {
+                    _userData = userData['users'];
+                  });
+                } else {
+                  // debugPrint("No user data received");
+                }
+              },
+              child: const Text('Get Email-UID Data'),
+            ),
+            if (_userData.isNotEmpty) ...[
+              const Text('First 10 User Data Objects:'),
+              ..._userData.sublist(0, 10).map((data) => Text(data.toString())),
             ],
           ],
         ),
